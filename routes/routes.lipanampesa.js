5e2b738bdcea3596c601a98003739f8710826d9e
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-10-17 15:30:41 +0000

    Shugli

--- routes/routes.lipanampesa.js
+++ routes/routes.lipanampesa.js
@@ -1,18 +1,13 @@
-
 import express from "express";
 const router = express.Router();
-import {
-  initiateSTKPush,
-  initiateB2CTransaction,
-  confirmPayment,
-} from "../controllers/controllers.lipanampesa.js";
+import { initiateSTKPush } from "../controllers/controllers.lipanampesa.js";
 import { accessToken } from "../middlewares/middlewares.generateAccessToken.js";
-import { apiKeyAuth } from '../middlewares/apiKeyAuth.js';
+import { apiKeyAuth } from "../middlewares/apiKeyAuth.js";
 
 router.route("/stkPush").post(apiKeyAuth, accessToken, initiateSTKPush);
-router.route("/withdraw/:userId").post(apiKeyAuth, accessToken, initiateB2CTransaction);
-router
-  .route("/confirmPayment/:CheckoutRequestID")
-  .post(accessToken, confirmPayment);
+// router.route("/withdraw/:userId").post(apiKeyAuth, accessToken, initiateB2CTransaction);
+// router
+//   .route("/confirmPayment/:CheckoutRequestID")
+//   .post(accessToken, confirmPayment);
 
 export default router;
